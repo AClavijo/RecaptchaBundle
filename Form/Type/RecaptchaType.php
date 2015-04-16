3a19b1f3667c5151e3bce66c43368b4483e086a3
Antoine Clavijo <antoine.clavijo@gmail.com>
2015-04-16 17:06:58 +0200

    wip

--- Form/Type/RecaptchaType.php
+++ Form/Type/RecaptchaType.php
@@ -2,16 +2,48 @@
 
 namespace AC\Bundle\RecaptchaBundle\Form\Type;
 
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\AbstractType;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * @author  Antoine Clavijo <antoine.clavijo@gmail.com>
  */
 class RecaptchaType extends AbstractType
 {
+    /**
+     * Recaptcha public key
+     * @var string
+     */
+    protected $publicKey;
+
+    /**
+     * Recaptcha private key
+     * @var string
+     */
+    protected $privateKey;
+
+    /**
+     * Google Recaptcha API
+     * @var string
+     */
+    protected $urlApi;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->publicKey  = $container->getParameter('ac_recaptcha.public_key');
+        $this->privateKey = $container->getParameter('ac_recaptcha.private_key');
+        $this->urlApi     = $container->getParameter('ac_recaptcha.url_api');
+    }
+
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-
+        $view->vars = array(
+            'publicKey' => $this->publicKey,
+            'url_api' => $this->urlApi,
+        );
     }
 
     /**
