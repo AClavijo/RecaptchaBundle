69ed4617e0c2b3591a3e3dc102356c22b4e16af4
Antoine Clavijo <antoine.clavijo@gmail.com>
2015-04-17 15:16:50 +0200

    wip

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AC\Bundle\Recaptcha\DependencyInjection;
+namespace AC\Bundle\RecaptchaBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
